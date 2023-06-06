da16940ca519823b61f2e7c24a3dc03309709c75
sudoLife <sudoLife1@proton.me>
2023-06-06 15:57:41 +0200

    Add huggingface pipeline for summarization

---
+++ run_summarization.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+python run_summarization.py \
+--model_name_or_path t5-small \
+--do_train \
+--do_eval \
+--dataset_name cnn_dailymail \
+--dataset_config "3.0.0" \
+--source_prefix "summarize: " \
+--output_dir ./tst-summarization \
+--per_device_train_batch_size=16 \
+--per_device_eval_batch_size=16 \
+--overwrite_output_dir \
+--predict_with_generate \
+--max_source_length 1024 \
+--max_target_length 128 \
